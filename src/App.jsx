ce3e982ab928b6c6f67f25fa885248324b42da90
Geremias Grassi <geremias.grassi@gmail.com>
2024-09-12 19:44:49 -0300

    Tercer-Commit
    Cumpli las consignas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import NavBar from './components/Navbar'
+import ItemListContainer from './components/ItemListContainer'
 
 function App() {
   return (
     <>
     <div className="App">
       <NavBar />
+      <ItemListContainer greeting={'Bienvenidos a la e-commerce N°1 del mundo!!'}/>
     </div>
     </>
   )
--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,5 +1,9 @@
 import './NavBar.css';
 
 export default function CartWidget() {
-    <button className='btn btn-links'>🛒3</button>
+    return (
+        <>
+        <button className='btn btn-links'>🛒3</button>
+        </>
+    )
     }
