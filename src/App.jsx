8a2a45f0145e8fde7d5aff3f13a6e83937ecd082
Geremias Grassi <geremias.grassi@gmail.com>
2024-09-24 19:32:09 -0300

    React Router Dom
    LE agregue el router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
 import NavBar from './components/Navbar'
+import { BrowserRouter, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <>
+    <BrowserRouter>
     <div className="App">
       <NavBar />
+      <Routes>
+        
+      </Routes>
     </div>
+    </BrowserRouter>
     </>
   )
 }
